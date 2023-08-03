b88d4d65c11719a452815900545b6012184ec5a0
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-08-03 10:46:45 -0500

    Infrastructure present, not test and update.

--- src/components/utilities/FetchForHomePage.js
+++ src/components/utilities/FetchForHomePage.js
@@ -145,6 +145,157 @@ const FetchForHomepage = () => {
         fetchNotifications();
     }, [])
 
+    // useEffect: fetchPayments
+    useEffect(() => {
+        async function fetchPayments () {
+            try {
+                const response = await fetch (`${BASE_URL}/api/payments`, {
+                    headers: {
+                        'Content-Type' : 'application/json'
+                    }
+                })
+                console.log("Response", response)
+                const paymentsData = await response.json();
+                console.log("Translated Payment Data:", paymentsData);
+                setPayments(paymentsData);
+            } catch (error) {
+                console.log ("Error fetching Payment Data!")
+                console.log(error)
+            }
+        }
+        fetchPayments();
+    }, [])
+
+    // useEffect: fetchPromotions
+    useEffect(() => {
+        async function fetchPromotions () {
+            try {
+                const response = await fetch (`${BASE_URL}/api/promotions`, {
+                    headers: {
+                        'Content-Type' : 'application/json'
+                    }
+                })
+                console.log("Response", response)
+                const promotionsData = await response.json();
+                console.log("Translated Promotion Data:", promotionsData);
+                setPromotions(promotionsData);
+            } catch (error) {
+                console.log ("Error fetching Promotion Data!")
+                console.log(error)
+            }
+        }
+        fetchPromotions();
+    }, [])
+
+    // useEffect: fetchReviews
+    useEffect(() => {
+        async function fetchReviews () {
+            try {
+                const response = await fetch (`${BASE_URL}/api/reviews`, {
+                    headers: {
+                        'Content-Type' : 'application/json'
+                    }
+                })
+                console.log("Response", response)
+                const reviewData = await response.json();
+                console.log("Translated Reviews Data:", reviewData);
+                setReviews(reviewData);
+            } catch (error) {
+                console.log ("Error fetching Reviews Data!")
+                console.log(error)
+            }
+        }
+        fetchReviews();
+    }, [])
+
+    // useEffect: fetchSalonRenters
+    useEffect(() => {
+        async function fetchSalonRenters () {
+            try {
+                const response = await fetch (`${BASE_URL}/api/renter`, {
+                    headers: {
+                        'Content-Type' : 'application/json'
+                    }
+                })
+                console.log("Response", response)
+                const salonRentersData = await response.json();
+                console.log("Translated Salon Renter Data:", salonRentersData);
+                setSalonRenters(salonRentersData);
+            } catch (error) {
+                console.log ("Error fetching Salon Renter Data!")
+                console.log(error)
+            }
+        }
+        fetchSalonRenters();
+    }, [])
+
+    // useEffect: fetchSalonSuites
+    useEffect(() => {
+        async function fetchSalonSuites () {
+            try {
+                const response = await fetch (`${BASE_URL}/api/suites`, {
+                    headers: {
+                        'Content-Type' : 'application/json'
+                    }
+                })
+                console.log("Response", response)
+                const salonSuitesData = await response.json();
+                console.log("Translated Salon Suites Data:", salonSuitesData);
+                setSalonRenters(salonSuitesData);
+            } catch (error) {
+                console.log ("Error fetching Salon Suites Data!")
+                console.log(error)
+            }
+        }
+        fetchSalonSuites();
+    }, [])
+
+    // useEffect: fetchServices
+    useEffect(() => {
+        async function fetchServices () {
+            try {
+                const response = await fetch (`${BASE_URL}/api/services`, {
+                    headers: {
+                        'Content-Type' : 'application/json'
+                    }
+                })
+                console.log("Response", response)
+                const serviceData = await response.json();
+                console.log("Translated Service Data:", serviceData);
+                setSalonRenters(serviceData);
+            } catch (error) {
+                console.log ("Error fetching Service Data!")
+                console.log(error)
+            }
+        }
+        fetchServices();
+    }, [])
+
+    // useEffect: fetchUser
+    useEffect(() => {
+        if(!localStorage.getItem("token")) {
+            navigate("/login")
+        }
+        async function fetchUser () {
+            try {
+                const response = await fetch (`${BASE_URL}/api/users`, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": `Bearer ${localStorage.getItem("token")}`
+                    }
+                })
+                console.log("Response", response)
+                const userData = await response.json();
+                console.log("Translated User Data:", userData);
+                setSalonRenters(userData.user);
+            } catch (error) {
+                console.log ("Error fetching Usser Data!")
+                console.log(error)
+            }
+        }
+        fetchUser();
+    }, [])
+
 // Return
     return(
         <div>
