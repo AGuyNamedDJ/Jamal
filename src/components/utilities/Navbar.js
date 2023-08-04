65407eb5a65c169bb48b1eb42f36f067c189d967
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-08-04 15:45:32 -0500

    Css getting better,

--- src/components/utilities/Navbar.js
+++ src/components/utilities/Navbar.js
@@ -6,12 +6,16 @@ import { Link } from "react-router-dom";
 const Navbar = ({}) => {
     return (
         <nav className="navbar">
-            <Link to="/locations" className="navbar-text">Locations</Link>
-            <Link to="/book" className="navbar-text">Book a Service</Link>
+            <div className="left-group">
+                <Link to="/locations" className="navbar-text">Locations</Link>
+                <Link to="/book" className="navbar-text">Book a Service</Link>
+            </div>
             <Link to="/" id="navbar-text">Home</Link>
-            <Link to="/join" className="navbar-text">Join Us</Link>
-            <Link to="/search" className="navbar-text">Search</Link>
-            <Link to="/account" className="navbar-text">Accounts</Link>
+            <div className="right-group">
+                <Link to="/join" className="navbar-text">Join Us</Link>
+                <Link to="/search" className="navbar-text">Search</Link>
+                <Link to="/account" className="navbar-text">Accounts</Link>
+            </div>
         </nav>
     )
 };
