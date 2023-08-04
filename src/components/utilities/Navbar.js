271a0ec7ebac75fa618c01b97d2fabceb76810c1
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-08-04 14:58:53 -0500

    Updating.

--- src/components/utilities/Navbar.js
+++ src/components/utilities/Navbar.js
@@ -6,8 +6,12 @@ import { Link } from "react-router-dom";
 const Navbar = ({}) => {
     return (
         <nav className="navbar">
+            <Link to="/locations" className="navbar-text">Locations</Link>
+            <Link to="/book" className="navbar-text">Book a Service</Link>
             <Link to="/" id="navbar-text">Home</Link>
-            
+            <Link to="/join" className="navbar-text">Join Us</Link>
+            <Link to="/search" className="navbar-text">Search</Link>
+            <Link to="/account" className="navbar-text">Accounts</Link>
         </nav>
     )
 };
