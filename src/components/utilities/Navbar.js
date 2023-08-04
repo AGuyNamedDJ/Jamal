c8b4c69943e01f75a8bc20cba93093629ac81bd8
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-08-04 16:27:49 -0500

    Navbar complete.

--- src/components/utilities/Navbar.js
+++ src/components/utilities/Navbar.js
@@ -1,6 +1,8 @@
 // Imports 
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 // Component
 const Navbar = ({}) => {
@@ -8,13 +10,19 @@ const Navbar = ({}) => {
         <nav className="navbar">
             <div className="left-group">
                 <Link to="/locations" className="navbar-text">Locations</Link>
+                <Link to="/about" className="navbar-text">About</Link>
                 <Link to="/book" className="navbar-text">Book a Service</Link>
             </div>
-            <Link to="/" id="navbar-text">Home</Link>
+            <Link to="/" id="navbar-text">JAMAL</Link>
             <div className="right-group">
+                <Link to="/search" className="navbar-text">
+                    <button className="search-button">
+                        <FontAwesomeIcon icon={faSearch} /> <span className="search-text"> Search</span>
+                    </button>
+                </Link>
                 <Link to="/join" className="navbar-text">Join Us</Link>
-                <Link to="/search" className="navbar-text">Search</Link>
-                <Link to="/account" className="navbar-text">Accounts</Link>
+                <Link to="/account" className="navbar-text">MyJamāl</Link>
+
             </div>
         </nav>
     )
