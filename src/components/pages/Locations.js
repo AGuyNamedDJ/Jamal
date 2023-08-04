50bc05b78443217c7d80622dc55655ccab4371d0
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-08-04 14:43:53 -0500

    Page updated and renders.

--- src/components/pages/Locations.js
+++ src/components/pages/Locations.js
@@ -7,14 +7,22 @@ const Locations = () => {
     const { franchiseLocationsState: [franchiseLocations, setFranchiseLocations] } = useOutletContext();
     return (
         <div>
-            <p >Locations</p>
-            {
-                franchiseLocations && franchiseLocations.length ? 
-                    <div>
-                        <p>{franchiseLocations[0].name}</p>
-                    </div>
-                    : "No Locations to display!"
-            }
+            <h1>Locations</h1>
+            {franchiseLocations.map((location, index) => (
+                <div key={index}>
+                    <h2>
+                    <Link to={`/locations/${location.id}`}>
+                        {location.name}
+                    </Link>
+                    </h2>
+                    <img src={location.imageUrl} alt={location.name} />
+                    <p>{location.address}</p>
+                    <p>{location.city}, {location.state} {location.zip_code}</p>
+                    <p>{location.country}</p>
+                    <p>Phone: {location.phone_number}</p>
+                    <p>Email: {location.email}</p>
+                </div>
+            ))}
         </div>
     );
 };
