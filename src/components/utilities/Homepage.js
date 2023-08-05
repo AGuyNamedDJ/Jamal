f71bb2e46f2ba184636b5fcb33fb1c9da30bc2f1
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-08-05 13:52:09 -0500

    Almost complete with hero section.

--- src/components/utilities/Homepage.js
+++ src/components/utilities/Homepage.js
@@ -1,11 +1,23 @@
 // Import
 import React from "react";
+import ChicagoRiver from "../videos/ChicagoRiver.mp4";
+import Chicago1 from "../videos/Chicago1.mp4";
+import { Link } from 'react-router-dom';
 
 // Functional Component
 const Homepage = () => {
     return(
-        <div>
-            <p>Here is your Homepage Welcome!</p>
+        <div className="hero-section">
+            <video autoPlay loop muted>
+                <source src={Chicago1} type="video/mp4" /> 
+            </video>
+            <div className="hero-content">
+                <h1>Our Flagship: Chicago Awaits</h1>
+                <div className="hero-buttons">
+                    <Link to="/link1" className="hero-button">Explore Services</Link>
+                    <Link to="/link2" className="hero-button">Join Us</Link>
+                </div>
+            </div>
         </div>
     )
 };
