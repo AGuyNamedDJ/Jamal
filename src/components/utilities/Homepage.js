b09fb7494e4d5639c752e198ca050cd00c10c357
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-08-05 19:08:49 -0500

    Backtrack to video.

--- src/components/utilities/Homepage.js
+++ src/components/utilities/Homepage.js
@@ -1,21 +1,13 @@
 // Import
-import React, { useRef, useEffect } from "react";
+import React from "react";
 import Chicago1 from "../videos/Chicago1.mp4";
 import { Link } from 'react-router-dom';
 
 // Functional Component
 const Homepage = () => {
-    const videoRef = useRef(null);
-
-    useEffect(() => {
-        if (videoRef.current) {
-            videoRef.current.play();
-        }
-    }, []);
-
     return(
         <div className="hero-section">
-            <video ref={videoRef} loop muted>
+            <video autoPlay loop muted>
                 <source src={Chicago1} type="video/mp4" /> 
             </video>
             <div className="hero-content">
@@ -26,7 +18,7 @@ const Homepage = () => {
                 </div>
             </div>
         </div>
-    );
+    )
 };
 
 // Export
