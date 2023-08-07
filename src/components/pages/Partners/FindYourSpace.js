8a45afee9f7164f1584151164e05d8f63d0e1c7e
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-08-07 14:26:06 -0500

    Data renders correctly. now we need to format.

--- src/components/pages/Partners/FindYourSpace.js
+++ src/components/pages/Partners/FindYourSpace.js
@@ -1,7 +1,9 @@
 // Import
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link, useOutletContext } from "react-router-dom";
 
 const FindYourSpace = () => {
+    const { franchiseLocationsState: [franchiseLocations, setFranchiseLocations] } = useOutletContext();
     return (
         <div class="webpage-container">
             
@@ -19,39 +21,41 @@ const FindYourSpace = () => {
                     </div>
                 </div>
             </div>
-
             {/* Locations Section */}
-            <div class="locations-section">
-                <div class="locations-title">
+            <div className="locations-section">
+                <div className="locations-title">
                     <h2>Our Locations</h2>
-                    <div class="divider"></div>
+                    <div className="divider"></div>
                 </div>
 
-                <div class="location">
-                    <div class="location-info">
-                    <h3>Location Name</h3>
-                    <p>Address Line 1</p>
-                    <p>Address Line 2</p>
-                    <a href="#" class="availability-link">Request Availability</a>
+                {franchiseLocations.map((location, index) => (
+                    <div className="location" key={location.id}>
+                        <div className="location-info">
+                            <h3>{location.name}</h3>
+                            <p>{location.address}</p>
+                            <Link to={`/location-details/${index}`} className="availability-link">
+                                <button>Request Availability</button>
+                            </Link>
+                        </div>
+                        <div className="location-images">
+                            <div className="image-container">
+                                <h4>Image Title 1</h4>
+                                <img src="image1.jpg" alt="Image Description 1" />
+                            </div>
+                            <div className="image-container">
+                                <h4>Image Title 2</h4>
+                                <img src="image2.jpg" alt="Image Description 2" />
+                            </div>
+                        </div>
+                        <div className="location-development">
+                            <h4>The Development</h4>
+                            <p>Description about this location.</p>
+                        </div>
                     </div>
-                    <div class="location-images">
-                    <div class="image-container">
-                        <h4>Image Title 1</h4>
-                        <img src="image1.jpg" alt="Image Description 1" />
-                    </div>
-                    <div class="image-container">
-                        <h4>Image Title 2</h4>
-                        <img src="image2.jpg" alt="Image Description 2" />
-                    </div>
-                    </div>
-                    <div class="location-development">
-                    <h4>The Development</h4>
-                    <p>Description about this location.</p>
-                    </div>
-                </div>
+                ))}
             </div>
         </div>
-    )
+    );
 };
 
 
