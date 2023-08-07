46878080c594c21916af342fce696cb5ff4d92e0
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-08-07 18:46:42 -0500

    Form complete.

--- src/components/pages/Partners/Contact.js
+++ src/components/pages/Partners/Contact.js
@@ -20,31 +20,34 @@ const Contact = () => {
         <div className="contact-page">
             <section className="contact-intro">
                 <h2>Get in Touch</h2>
-                <p>Indulge in the distinction of the Jamāl community, an exclusive haven for the discerning professional. Our space embodies elegance and empowerment, enabling you to elevate your craft, flourish on your terms, and achieve unparalleled success.</p>
-                <p>To embark on this journey with us, or to inquire further about the opportunities that await, we invite you to complete the form below. Alternatively, you may reach us through email or telephone.</p>
+                <p>To embark on this journey with us, or to inquire further about the opportunities that await, we invite you to complete the form below.</p>
+                <p>.</p>
+                <p>If interested in joining or to receive more information, fill out the form below, email or call us.</p>
+                <p>email: <a href="mailto:dalronjrobertson@gmail.com">dalronjrobertson@gmail.com</a></p>
+                <p>phone: <a href="tel:708-xxx-xxxx">708-xxx-xxxx</a> or <a href="#">text message</a></p>
             </section>
             <section className="contact-form">
                 <form onSubmit={handleSubmit}>
                     <div className="form-group">
                         <div className="name-field">
-                            <label htmlFor="firstName">First Name:</label>
+                            <label htmlFor="firstName">First Name: <span className="required-text">(required)</span></label>
                             <input type="text" id="firstName" name="firstName" required />
                         </div>
                         <div className="name-field">
-                            <label htmlFor="lastName">Last Name:</label>
+                            <label htmlFor="lastName">Last Name: <span className="required-text">(required)</span></label>
                             <input type="text" id="lastName" name="lastName" required />
                         </div>
                     </div>
                     <div className="form-group">
-                        <label htmlFor="email">Email:</label>
+                        <label htmlFor="email">Email: <span className="required-text">(required)</span></label>
                         <input type="email" id="email" name="email" required />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="phone">Phone:</label>
+                        <label htmlFor="phone">Phone: <span className="required-text">(required)</span></label>
                         <input type="tel" id="phone" name="phone" required />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="servicesOffered">Services Offered:</label>
+                        <label htmlFor="servicesOffered">Services Offered: <span className="required-text">(required)</span></label>
                         <select id="servicesOffered" name="servicesOffered" required>
                             <option value="">Select a Service</option>
                             <option value="service1">Barber</option>
@@ -59,16 +62,29 @@ const Contact = () => {
                     </div>
                     <div className="form-group">
                         <label htmlFor="interestedLocations">Interested Locations:</label>
-                        <input type="text" id="interestedLocations" name="interestedLocations" required />
+                        <div id="interestedLocations">
+                            <div className="location-option">
+                                <label>
+                                    <input type="checkbox" name="interestedLocations" value="Location1" />
+                                    <span>Chicago: South Loop</span>
+                                </label>
+                            </div>
+                            <div className="location-option">
+                                <label>
+                                    <input type="checkbox" name="interestedLocations" value="Location1" />
+                                    <span>Future Location</span>
+                                </label>
+                            </div>
+                        </div>
                     </div>
                     <div className="form-group">
-                        <label htmlFor="howDidYouFindUs">How Did You Find Us?</label>
+                        <label htmlFor="howDidYouFindUs">How Did You Find Us? <span className="required-text">(required)</span></label>
                         <input type="text" id="howDidYouFindUs" name="howDidYouFindUs" required />
                     </div>
                     <button type="submit" className="send-button">Send</button>
                 </form>
-        </section>
-    </div>
+            </section>
+        </div>
     );
 };
 
