4140e4fb119b6b62709985a11f6bb88698f6016a
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-08-07 13:28:13 -0500

    1st section complete.

--- src/components/pages/Partners/FindYourSpace.js
+++ src/components/pages/Partners/FindYourSpace.js
@@ -3,8 +3,15 @@ import React from "react";
 
 const FindYourSpace = () => {
     return(
-        <div>
-            <p>Here is your find your space page</p>
+        <div class="intro-section">
+            <div class="text-content">
+                <h1>Join Jamal</h1>
+                <p>Embrace your calling; forge unparalleled luxury. At Jamal, your artistry becomes an expression of elegance and sophistication.</p>
+                <p>Join the Jamal collective, where grace meets magnificence. Thrive on your terms, transforming the ordinary into the extraordinary, in a setting as distinctive as your craft.</p>
+            </div>
+            <div class="video-content">
+                <iframe src="https://www.youtube.com/embed/vqk4LJB6SGk?autoplay=1&mute=1&loop=1&playlist=vqk4LJB6SGk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
         </div>
     )
 };
