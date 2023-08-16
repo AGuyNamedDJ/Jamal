7c1c3a4463a6ab2568f1535cc79fded125783154
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-08-16 15:27:06 -0500

    Structure complete.

--- src/components/pages/Customers/Locations.js
+++ src/components/pages/Customers/Locations.js
@@ -10,18 +10,25 @@ const Locations = () => {
                 <h1 className="FranchiseLocations-title">Our Locations</h1>
                 <div className="FranchiseLocations-divider"></div>
                 {franchiseLocations.map((location, index) => (
-                <div key={index} className="FranchiseLocations-locationCard">
-                    <h2>{location.name}</h2>
-                    <p>{location.address}</p>
-                    <div class="franchise-video-content">
-                        <iframe src="https://www.youtube.com/embed/vqk4LJB6SGk?autoplay=1&mute=1&loop=1&playlist=vqk4LJB6SGk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div key={index}>
+                        <div className="FranchiseLocations-locationCard">
+                            <div className="FranchiseLocations-info">
+                                <h2>{location.name}</h2>
+                                <p>{location.address}</p>
+                                <p>Building Access Hours: {location.business_hours}</p>
+                            </div>
+                            <div className="franchise-video-content">
+                                <iframe src="https://www.youtube.com/embed/vqk4LJB6SGk?autoplay=1&mute=1&loop=1&playlist=vqk4LJB6SGk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
+                        </div>
+                    <div className="FranchiseLocations-detailButtonContainer">
+                        <Link to={`/locations/${location.id}`} className="FranchiseLocations-detailButton">
+                            <button>View Location</button>
+                        </Link>
                     </div>
-                    <Link to={`/locations/${location.id}`} className="FranchiseLocations-detailButton">
-                        <button>Location Details</button>
-                    </Link>
                 </div>
             ))}
-        </div>
+      </div>
     );
 };
 
