0655753433b34b8573ef40f209c130fa768f0f72
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-08-28 17:21:36 -0500

    Renders properly, structure setup basics.

--- src/components/pages/Customers/LocationsDetail.js
+++ src/components/pages/Customers/LocationsDetail.js
@@ -1,13 +1,55 @@
 // Import
-import React from "react";
+import React, { useState, useEffect} from "react";
+import { useNavigate, useParams } from "react-router-dom";
+// import { useOutletContext } from "your-context-file"; // Uncomment if you're using it
 
-const LocationsDetial = () => {
-    return(
+// Functional Component
+const LocationsDetail = () => {
+    // const { franchiseLocationsState: [franchiseLocations, setFranchiseLocations] } = useOutletContext(); // Uncomment if you're using it
+    const navigate = useNavigate();
+    const {id} = useParams();
+    const [locationDetail, setLocationDetail] = useState({});
+    const BASE_URL = "https://jamal-backend.onrender.com";
+
+    // fetchLocationDetail
+    useEffect (() => {
+        async function fetchLocationDetail(){
+            try {
+                const response = await fetch (`${BASE_URL}/api/locations/${id}`,{
+                    headers: { 'Content-Type' : 'application/json' }
+                });
+                const locationSpecific = await response.json();
+                setLocationDetail(locationSpecific);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchLocationDetail(); // Fixed function name
+    }, [id]);
+
+    return (
         <div>
-            <p>Here is your LocationsDetial page</p>
+            <div id="location-details">
+                {/* Name */}
+                {
+                    locationDetail.name ? // Fixed variable name
+                    <h1>{locationDetail.name}</h1> :
+                    <p>Name not available</p>
+                }
+                {/* Address */}
+                {
+                    locationDetail.address ? // Fixed variable name
+                    <p>{locationDetail.address}</p> :
+                    <p>Address not available</p>
+                }
+                {/* Other Details ... */}
+            </div>
+            
+            {/* Go Back Button */}
+            <button onClick={() => navigate(-1)} id="go-back-button">Back</button>
         </div>
-    )
+    );
 };
 
 // Export
-export default LocationsDetial;
+export default LocationsDetail;
